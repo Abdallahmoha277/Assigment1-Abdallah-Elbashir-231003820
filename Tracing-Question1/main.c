5f9d04b788ba9f5cdd9e8edfab82f2dfb6dd67b5
Abdallahmoha277 <156453865+Abdallahmoha277@users.noreply.github.com>
2025-03-28 13:48:10 +0200

    Update main.c

--- Tracing-Question1/main.c
+++ Tracing-Question1/main.c
@@ -3,17 +3,15 @@
 int main() {
     int a = 10, b = 5, sum, product, difference;
 
-    // Perform calculations
     sum = a + b;
     product = a * b;
     difference = a - b;
-
-    // Output results
+    
     printf("Sum: %d\n", sum);
     printf("Product: %d\n", product);
     printf("Difference: %d\n", difference);
 
-    // Conditional check
+    
     if (sum > product) {
         printf("Sum is greater than Product\n");
     } else {
