4c6f3af254b5750a1daf1a1d9e5bfd8188001338
Abdallahmoha277 <156453865+Abdallahmoha277@users.noreply.github.com>
2025-03-28 13:46:18 +0200

    Update main.c

--- Palindrome-Number-Files/main.c
+++ Palindrome-Number-Files/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int isPalindrome(int x) {
+    if (x < 0) return 0;
+
+    int original = x;
+    int reversed = 0;
+
+    while (x != 0) {
+        int digit = x % 10;            
+        reversed = reversed * 10 + digit;
+        x = x / 10;                    
+    }
+    if (original == reversed)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (isPalindrome(number))
+        printf("True: The number is a palindrome.\n");
+    else
+        printf("False: The number is NOT a palindrome.\n");
+
     return 0;
 }
