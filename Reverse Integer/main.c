1bc12523850d1158ec16f18192eaa34f2851999c
Abdallah Adam <156453865+Abdallahmoha277@users.noreply.github.com>
2025-04-03 20:53:16 +0200

    Update main.c

--- Reverse Integer/main.c
+++ Reverse Integer/main.c
@@ -1,31 +1,15 @@
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
 
-int reverseInteger(int x) {
-    int reversed = 0;
-
-    while (x != 0) {
-        int digit = x % 10;
-
-        if (reversed > INT_MAX / 10 || (reversed == INT_MAX / 10 && digit > 7))
-            return 0;
-        if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && digit < -8))
-            return 0;
-
-        reversed = reversed * 10 + digit;
-        x /= 10;
+int main(){
+    int i, x, digit, reverse = 0;
+    printf("Enter the number : ");
+    scanf("%d", &x);
+    while(x!=0){
+    digit = x%10;
+    x = x / 10;
+    reverse = reverse * 10  + digit;
     }
-
-    return reversed;
-}
-
-int main() {
-    int num;
-
-    printf("Enter an integer: ");
-    scanf("%d", &num);
-
-    printf("Reversed integer: %d\n", reverseInteger(num));
-
+    printf("The reverse number is  : %d", reverse);
     return 0;
 }
